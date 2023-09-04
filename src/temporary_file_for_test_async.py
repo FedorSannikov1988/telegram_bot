2d7aed638ba688e8644a97ac77f21d0dda8c3a20
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-09-04 19:39:44 +0300

    work async

---
+++ src/temporary_file_for_test_async.py
@@ -0,0 +1,28 @@
+import asyncio
+import time
+
+
+async def fun1(x):
+    print('fun1 начата')
+    print(x**2)
+    await asyncio.sleep(10)
+    print('fun1 завершена')
+
+
+async def fun2(x):
+    print('fun2 начата')
+    print(x**0.5)
+    await asyncio.sleep(10)
+    print('fun2 завершена')
+
+
+async def main():
+    task1 = asyncio.create_task(fun1(4))
+    task2 = asyncio.create_task(fun2(4))
+    await asyncio.gather(task1, task2)
+
+print(time.strftime('%X'))
+
+asyncio.run(main())
+
+print(time.strftime('%X'))
